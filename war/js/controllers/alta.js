c57846f76bb24b1a190a5486fcdb756eb3cc6c44
Risfaza <mrfemers@mail.com>
2019-06-07 13:15:24 -0500

    Miles

--- war/js/controllers/alta.js
+++ war/js/controllers/alta.js
@@ -5,28 +5,32 @@ app.controller("alta",[
 	'$location',
 	'$window','sessionService',
 	function($scope,$routeParams,$http,$location, $window,sessionService){
+		$scope.btndis=false;
 		$scope.guardar = function(tipo){
+			$scope.btndis=true;
 			if(tipo=== undefined){
 				 document.getElementById("selected").focus();
+				 $scope.btndis=false;
 				 return;
 			}
 			if(tipo=="cliente"){
-				$http.get('/load/clientes',$scope.send).then(function(response) {
+				$http.post('/load/clientes',$scope.send).then(function(response) {
 					alert("Operacion Exitosa");
 					$scope.send = null
 				})
 			}
 			if(tipo=="cuenta"){
-				$http.get('/load/cuentas',$scope.send).then(function(response) {
+				$http.post('/load/cuentas',$scope.send).then(function(response) {
 					alert("Operacion Exitosa");
 					$scope.send = null
 				})
 			}
 			if(tipo=="empresa"){
-				$http.get('/load/empresas',$scope.send).then(function(response) {
+				$http.post('/load/empresas',$scope.send).then(function(response) {
 					alert("Operacion Exitosa");
 					$scope.send = null
 				})
 			}
+			$scope.btndis=false;
 		}
 	}]);
