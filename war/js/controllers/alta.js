7d25477d37cc99a2f8279966dabf8b7a9ca457fe
Risfaza <mrfemers@mail.com>
2019-06-07 12:15:47 -0500

    up

--- war/js/controllers/alta.js
+++ war/js/controllers/alta.js
@@ -11,19 +11,19 @@ app.controller("alta",[
 				 return;
 			}
 			if(tipo=="cliente"){
-				$http.post('/load/clientes',$scope.send).then(function(response) {
+				$http.get('/load/clientes',$scope.send).then(function(response) {
 					alert("Operacion Exitosa");
 					$scope.send = null
 				})
 			}
 			if(tipo=="cuenta"){
-				$http.post('/load/cuentas',$scope.send).then(function(response) {
+				$http.get('/load/cuentas',$scope.send).then(function(response) {
 					alert("Operacion Exitosa");
 					$scope.send = null
 				})
 			}
 			if(tipo=="empresa"){
-				$http.post('/load/empresas',$scope.send).then(function(response) {
+				$http.get('/load/empresas',$scope.send).then(function(response) {
 					alert("Operacion Exitosa");
 					$scope.send = null
 				})
